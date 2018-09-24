0246071305feca5e1562f9c13256e72b680319d1
anka <osmandurdag@hotmail.com>
2018-09-25 01:01:24 +0300

    Bos form .cpp ve .h

---
+++ EmptyForm/MyForm.cpp
@@ -0,0 +1,13 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	EmptyForm::MyForm form;
+	Application::Run(%form);
+	return 0;
+}
---
+++ EmptyForm/MyForm.h
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace EmptyForm {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// MyForm için özet
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Oluþturucu kodunu buraya ekle
+			//
+		}
+
+	protected:
+		/// <summary>
+		///Kullanýlan tüm kaynaklarý temizleyin.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		///Gerekli tasarýmcý deðiþkeni.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Tasarýmcý desteði için gerekli metot - bu metodun 
+		///içeriðini kod düzenleyici ile deðiþtirmeyin.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
+			this->Text = L"MyForm";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
