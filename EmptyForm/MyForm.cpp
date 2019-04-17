9df0b81e11c5a708ce8bb891e9301bb85c1dc3a5
Osman Durdağ <40799665+zumrudu-anka@users.noreply.github.com>
2019-04-17 14:26:48 +0300

    Update MyForm.cpp

--- EmptyForm/MyForm.cpp
+++ EmptyForm/MyForm.cpp
@@ -7,7 +7,7 @@ using namespace System::Windows::Forms;
 int main(array<String^>^ args) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-	EmptyForm::MyForm form;
+	EmptyForm::MyForm form;	// EmptyForm ifadesinin yerine proje adi yazilmali(X projesinin MyFormu gibi..MyForm ise olusturdugumuz formun adi)
 	Application::Run(%form);
 	return 0;
 }
